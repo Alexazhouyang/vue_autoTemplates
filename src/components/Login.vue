03ef115abcc725c41249d8f564d43f5ea49e0020
Alexazhouyang <alexazhouyang@gmail.com>
2018-07-01 17:31:32 +0800

    update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,18 @@
 <template>
-    <div id="login">
-        <el-container>
-<el-card class="box-card">
-  <h2>Login</h2>
+<div id="login" style="width:100%">   
+<el-card class="box-card" >
+   <img src="../assets/logo.png"/>
+   <h2>Login</h2>
   <el-form>
       <el-form-item label="User">
       <el-input placeholder="user name" v-model="username"/></el-form-item>
       <el-form-item label="Password">
       <el-input placeholder="password" v-model="password"/></el-form-item>
       <el-form-item>
-      <el-button @click="selectUser" class="" >SUBMIT</el-button></el-form-item>
+      <el-button type="primary" @click="selectUser" >SUBMIT</el-button></el-form-item>
   </el-form>
 </el-card>
-        </el-container>
+
     </div>
 </template>
 <script>
@@ -22,19 +22,24 @@ export default {
         return{
             username : '',
             password : '',
+            list:[],
         }
     },
     methods:{
         selectUser(){
-
+            this.$router.push({
+                path: '/Product',
+               })
         },
     },
 }
 </script>
 <style  scoped>
 #login{
+   
+    margin: 0 10% ;
     text-align: center;
-
+    background-color: #2c3e50;
 }
 </style>
 
